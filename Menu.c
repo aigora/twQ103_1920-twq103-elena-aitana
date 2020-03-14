e5722b63b29daca7fa5a23ee1b0f7e40bf332a1a
Aitana-C <48099611+Aitana-C@users.noreply.github.com>
2020-03-14 12:39:12 +0100

    Create Menu.c
    Se añade el menú de opciones del que dispone la tienda virtual.

---
+++ Menu.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	int opcion;
+	
+	printf("                            Bienvenido/a a ETSIDICHEMISTLAB\n");
+	printf("Tu tienda online donde podras encontrar el producto quimico que tanto tiempo has estado buscando\n");
+	printf("A continuacion te mostramos el menu de opciones:\n");
+	printf("1. Registro.\n");
+	printf("2. Stock.\n");
+	printf("3. Venta.\n");
+	scanf("%d", &opcion);
+	
+	return 0;
+}
